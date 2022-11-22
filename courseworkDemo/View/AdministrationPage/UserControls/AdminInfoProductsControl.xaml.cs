94502d84fc9b892f7bbf772c52ff98147e2fa82b
Павел Гаврютченков <ugfof@mail.ru>
2022-11-22 21:31:49 +0300

    LogicsAdminPage

--- courseworkDemo/View/AdministrationPage/UserControls/AdminInfoProductsControl.xaml.cs
+++ courseworkDemo/View/AdministrationPage/UserControls/AdminInfoProductsControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using courseworkDemo.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,35 @@ namespace courseworkDemo.View.AdministrationPage.UserControls
         public AdminInfoProductsControl()
         {
             InitializeComponent();
+        }
+
+        private async void BtnRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(TbProductsName.Text) || string.IsNullOrEmpty(TbPrice.Text))
+            {
+                MessageBox.Show("Все поля должны быть заполнены!", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if(FrameNavigate.DB.Products.Count(u => u.ProductsName == TbProductsName.Text) > 0)
+                {
+                    MessageBox.Show("Продукт с таким названием уже есть!", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                FrameNavigate.DB.Products.Add(new Model.Product
+                {
+                    ProductsName = TbProductsName.Text,
+                    ProductsPrice = TbPrice.Text
+                });
+
+                await FrameNavigate.DB.SaveChangesAsync();
+                MessageBox.Show("Продукт добвален!", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                FrameNavigate.FrameObject.Navigate(new MainAdministratorPage());
+            }
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            FrameNavigate.FrameObject.Navigate(new MainAdministratorPage());
         }
     }
 }
